2b44fa6846920b624760ebe5089139703b573704
Boris Tayourksy <tayoursky@gmail.com>
2022-02-05 13:18:13 +0300

    change settings docker

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test.loc</title>
+    <title>Default page</title>
 </head>
 <body>
-<div class="container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis culpa cumque delectus dicta dolor dolores eaque eligendi eos exercitationem facere minima nisi, numquam odio ratione saepe tenetur vero vitae!</div>
+<?= phpinfo(); ?>
 </body>
 </html>
 
